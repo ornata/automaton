2b01265832bffbc55efb8f2debfa31c8aef0e661
Jessica Paquette <jlpaquet@uvic.ca>
2015-07-26 14:33:12 -0700

    made it even shorter and fixed some inconsistent style

--- automaton.c
+++ automaton.c
@@ -8,43 +8,30 @@
 #define NCOLS 201
 
 /* Prints out the cellular automaton */
-void print_grid(int8_t grid[NROWS][NCOLS], FILE* ofp)
+void print_grid(uint8_t grid[NROWS][NCOLS], FILE* ofp)
 {
     int i, j;
-    for (i = 0; i < NROWS; i++)
-    {
-        for (j = 1; j < NCOLS-1; j++)
-        {
+    
+    for (i = 0; i < NROWS; i++) {
+        for (j = 1; j < NCOLS-1; j++) {
             fprintf(ofp, "%d ", grid[i][j]);
         }
         fprintf(ofp, "\n");
     }
 }
 
-/* Decides the colour for the cell directly below grid[i][j] based
-* off 
-*     1. The binary value that left, centre and right form
-*     2. Whether or bot the corresponding bit in rule is set or not.
-*/
-int eval_middle(int8_t grid[NCOLS], int index, int8_t rule)
-{
-    int8_t left, right, centre, nbrs;
-    left = grid[index - 1];
-    right = grid[index + 1];
-    centre = grid[index];
-    nbrs = (left << 2) | (centre << 1) | (right);
-    return (rule >> nbrs) & 0x1;
-}
-
 /* Calls eval_middle to determine whether or not the current cell is
 * black (1) or white (0).
 */
-void build_automata(int8_t grid[NROWS][NCOLS], int8_t rule)
+void build_automata(uint8_t grid[NROWS][NCOLS], uint8_t rule)
 {
     int i, j;
+    int8_t nbrs;
+
     for (i = 0; i < NROWS-1; i++) {
         for (j = 1; j < NCOLS-1; j++) {
-            grid[i+1][j] = eval_middle(grid[i], j, rule);
+            nbrs = (grid[i][j - 1] << 2) | (grid[i][j] << 1) | (grid[i][j + 1]);
+            grid[i+1][j] = (rule >> nbrs) & 0x1;
         }
     }
 }
@@ -55,7 +42,7 @@ void build_automata(int8_t grid[NROWS][NCOLS], int8_t rule)
 * To increase automata size, increase NROWS or NCOLS. */
 int main(int argc, char* argv[])
 {
-    int8_t grid[NROWS][NCOLS];
+    uint8_t grid[NROWS][NCOLS];
     int i, j;
     uint8_t rule;
     FILE* ofp;
@@ -63,22 +50,19 @@ int main(int argc, char* argv[])
     fprintf(stdout, "Input an integer between 0 and 255.\n");
     scanf("%2" SCNu8, &rule);
 
-    // read in the rule from stdin
     for (i = 0; i < NROWS; i++)
         for (j = 0; j < NCOLS; j++)
             grid[i][j] = 0;
 
-    grid[0][NCOLS/2] = 1;
+    grid[0][NCOLS/2] = 1; // initial grid configuration
     build_automata(grid, rule);
-
     ofp = fopen("automaton.pbm", "w");
 
-    if (ofp == NULL)
-    {
+    if (ofp == NULL) {
         fprintf(stderr, "Could not create image.\n");
         exit(-1);
     }
-    
+
     fprintf(ofp, "P1\n");
     fprintf(ofp, "%d %d\n", NROWS-1, NCOLS-2);
     print_grid(grid, ofp);
